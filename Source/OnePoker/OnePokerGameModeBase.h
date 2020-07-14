51024449b1aeb074495802a6709f8369a8b9c667
misterq <illsi@naver.com>
2020-07-15 00:27:15 +0900

    Initial commit

---
+++ Source/OnePoker/OnePokerGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "OnePokerGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ONEPOKER_API AOnePokerGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
